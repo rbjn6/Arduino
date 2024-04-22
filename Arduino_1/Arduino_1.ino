088a45da24881ed1a341cd2464658c0ee52ed84b
rbjn6 <145556506+rbjn6@users.noreply.github.com>
2024-04-22 23:59:19 +0200

    Update Arduino_1.ino

--- Arduino_1/Arduino_1.ino
+++ Arduino_1/Arduino_1.ino
@@ -1,38 +1,127 @@
-void setup (){
-  pinMode (2, INPUT);
-  pinMode (6, INPUT);
-  pinMode (4, OUTPUT);
-  pinMode (3, OUTPUT);
-  pinMode (8, OUTPUT);
-  pinMode (7, OUTPUT);
+//____________DECLARATION DES VARIABLES EN LIEN AVEC LES MOTEURS____________
+const int moteurG=12;
+const int brakeG=9;
+
+const int moteurD=13;
+const int brakeD=8;
+
+//-----------DECLARATION DES VARIABLES EN LIEN AVEC LES MOUSTACHES-----------
+const int Moustache=1;
+
+//----------------SETUP----------------
+void setup() {
+  //Setup Channel G
+  pinMode(moteurG, OUTPUT); //Initiates Motor Channel G pin
+  pinMode(brakeG, OUTPUT); //Initiates Brake Channel G pin
+
+  //Setup Channel D
+  pinMode(moteurD, OUTPUT); //Initiates Motor Channel D pin
+  pinMode(brakeG, OUTPUT);  //Initiates Brake Channel D pin
 }
 
+//------------------FONCTIONS POUR LES MOTEURS------------------
+void avancer()
+{
+  digitalWrite(moteurG, LOW);
+  digitalWrite(brakeG, LOW);
+  analogWrite(3,255);
+  digitalWrite(moteurD, LOW);
+  digitalWrite(brakeD, LOW);
+  analogWrite(11,255);
+}
+
+void gauche()
+{
+  digitalWrite(moteurG, HIGH);
+  digitalWrite(brakeG, HIGH);
+  analogWrite(3,0);
+  digitalWrite(moteurD, LOW);
+  digitalWrite(brakeD, LOW);
+  analogWrite(11,255);
+}
+void droite()
+{
+  digitalWrite(moteurG, LOW);
+  digitalWrite(brakeG, LOW);
+  analogWrite(3,255);
+  digitalWrite(moteurD, HIGH);
+  digitalWrite(brakeD, HIGH);
+  analogWrite(11,0);
+}
+void reculer()
+{
+  digitalWrite(moteurG, HIGH);
+  digitalWrite(brakeG, LOW);
+  analogWrite(3,255);
+  digitalWrite(moteurD, HIGH);
+  digitalWrite(brakeD, LOW);
+  analogWrite(11,255);
+}
+void arreter()
+{
+  digitalWrite(moteurG, HIGH);
+  digitalWrite(brakeG, HIGH);
+  analogWrite(3,0);
+  digitalWrite(moteurD, HIGH);
+  digitalWrite(brakeD, HIGH);
+  analogWrite(11,0);
+}
+void pivoterGauche()
+{
+  digitalWrite(moteurG, HIGH);
+  digitalWrite(brakeG, LOW);
+  analogWrite(3,255);
+  digitalWrite(moteurD, LOW);
+  digitalWrite(brakeD, LOW);
+  analogWrite(11,255);
+}
+void pivoterDroite()
+{
+  digitalWrite(moteurG, LOW);
+  digitalWrite(brakeG, LOW);
+  analogWrite(3,255);
+  digitalWrite(moteurD, HIGH);
+  digitalWrite(brakeD, LOW);
+  analogWrite(11,255);
+
+}
+void pivoterGauche25()
+{
+  pivoterGauche();
+  delay(900);
+}
+void pivoterGauche50()
+{
+  pivoterGauche();
+  delay(1800);
+}
+void pivoterGauche100()
+{
+  pivoterGauche();
+  delay(3600);
+}
+void pivoterDroite25()
+{
+  pivoterDroite();
+  delay(900);
+}
+void pivoterDroite50()
+{
+  pivoterDroite();
+  delay(1800);
+}
+void pivoterDroite100()
+{
+  pivoterDroite();
+  delay(3600);
+}
+
+//---------------BOUCLE---------------
 void loop() {
-  if ((( digitalRead (2))==(1))){
-    # A realiser pendant un temps precis
-    digitalWrite (3,0);
-    digitalWrite(7,0);
-    digitalWrite (4,1);
-  }else if (((digitalRead (6))==(1))){
-    # A realiser pendant un temps precis
-    digitalWrite (3,0);
-    digitalWrite(7,0);
-    digitalWrite (8,1);
-  } else{
-    digitalWrite (4,0);
-    digitalWrite (8,0);
-    digitalWrite (3,1);
-    digitalWrite(7,1);
-  }
-  _loop();
-}
-
-void _delay(float seconds){
-  long endTime = millis() + seconds *1000;
-  while(millis()<endTime)_loop();
+  pivoterGauche100();
+  arreter();
+  delay(500);
 
 }
-
-void _loop(){
 
 }
